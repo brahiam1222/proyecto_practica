557eb7209a84e5ec86b78603f3ecfb636d1b204e
Brahiam Steven <brahiam-1222@hotmail.com>
2022-06-05 22:20:56 -0500

    Avance05061020

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -22,7 +22,7 @@
 <header class="p-3 mb-3 border-bottom">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+        <!-- <a href="index.php?pagina=inicio" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"> -->
           <svg class="bi me-2" width="40" height="32" role="img" aria-label="Logo"><use xlink:href="#logo"></use></svg>
         </a>
 
