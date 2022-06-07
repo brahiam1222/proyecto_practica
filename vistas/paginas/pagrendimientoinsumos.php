633089670e714ad89700ff753dacf3122242eaed
Brahiam Steven <brahiam-1222@hotmail.com>
2022-06-06 22:22:47 -0500

    Avance06061022

--- vistas/paginas/pagrendimientoinsumos.php
+++ vistas/paginas/pagrendimientoinsumos.php
@@ -11,4 +11,15 @@ if (!isset($_SESSION["validar"])) {
   }
 }
 
+
+
+?>
+
+<iframe title="Lluvias por mes - Página 1" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMDAxNjNhODAtMmQ2Yy00NzRlLThmMDEtODExMWUzYzRhMTcyIiwidCI6ImI5NDc0NWY2LTExYjgtNDlkYi04NTdmLTI0MDFhY2U3ZDViOCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+
+
+<?php
+
+
+
 ?>
