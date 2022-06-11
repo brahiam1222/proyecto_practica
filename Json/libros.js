4f445547a1903ffc0079df19ce95314d3e126e67
Brahiam Steven <brahiam-1222@hotmail.com>
2022-06-10 21:47:51 -0500

    Avance1006947

--- Json/libros.js
+++ Json/libros.js
@@ -1,39 +1,39 @@
-const titulo = document.getElementById('titulo');
-const autor = document.getElementById('autor');
-const editorial = document.getElementById('editorial');
-const publicado = document.getElementById('publicado');
-const isbn = document.getElementById('isbn');
-const categoria = document.getElementById('categoria');
-const precio = document.getElementById('precio');   
-const formulario = document.getElementById('formulario');
+// const titulo = document.getElementById('titulo');
+// const autor = document.getElementById('autor');
+// const editorial = document.getElementById('editorial');
+// const publicado = document.getElementById('publicado');
+// const isbn = document.getElementById('isbn');
+// const categoria = document.getElementById('categoria');
+// const precio = document.getElementById('precio');   
+// const formulario = document.getElementById('formulario');
 
-//enviar json a mysql
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let libro = {
-        titulo: titulo.value,
-        autor: autor.value,
-        editorial: editorial.value,
-        publicado: publicado.value,
-        isbn: isbn.value,
-        categoria: categoria.value,
-        precio: precio.value
-    }
-    fetch('/libros', {
-        method: 'POST',
-        body: JSON.stringify(libro),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        formulario.reset();
-    })
-    .catch(err => console.log(err));
-}
-);
+// //enviar json a mysql
+// formulario.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     let libro = {
+//         titulo: titulo.value,
+//         autor: autor.value,
+//         editorial: editorial.value,
+//         publicado: publicado.value,
+//         isbn: isbn.value,
+//         categoria: categoria.value,
+//         precio: precio.value
+//     }
+//     fetch('/libros', {
+//         method: 'POST',
+//         body: JSON.stringify(libro),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+//         formulario.reset();
+//     })
+//     .catch(err => console.log(err));
+// }
+// );
 
 //SELECT * FROM `datos`
 
