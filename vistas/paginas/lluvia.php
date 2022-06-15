7d0ef7f4f941b6d7f2dbeeea98fa1234d3e94eec
brahi <brahi@DESKTOP-8V1T4Q4>
2022-06-15 16:27:02 -0500

    Avance1506426

--- vistas/paginas/lluvia.php
+++ vistas/paginas/lluvia.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION["validar"])) {
 
 ?>
 
-<iframe title="Informe Precipitacion  y Sigatoka - Lluvia" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiNzlhNDZjN2ItYjY4NS00ZWY4LTliZDAtNzM2ODc0ZjFlNDI0IiwidCI6ImI5NDc0NWY2LTExYjgtNDlkYi04NTdmLTI0MDFhY2U3ZDViOCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="Informe Precipitacion  y Sigatoka - Lluvia" width="1300" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiNDEyOTYyN2ItYjU3Yi00ZjViLTg4ODgtMzQ5NWRiMzE0ZTNiIiwidCI6ImI5NDc0NWY2LTExYjgtNDlkYi04NTdmLTI0MDFhY2U3ZDViOCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
 
 
 <?php
