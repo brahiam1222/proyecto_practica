3d0923f844920943fc9d437f285144ca0b55aa0f
Brahiam Steven <brahiam-1222@hotmail.com>
2022-07-19 21:09:08 -0500

    prueba

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    prueba
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
  <?php 
 
     
@@ -29,7 +7,4 @@
     require_once "modelo/formulario-modelo.php";
 
     $plantilla = new ControladorInicio();
-    $plantilla -> ctrInicio()
-    
-    
-
+    $plantilla -> ctrInicio();
